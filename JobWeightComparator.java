3633bf6641a2b4f19cda1fb1387533a9e4d68eb3
MandoMirsh <chsn95@mail.ru>
2022-05-03 23:30:00 +0300

    Added Comarator and sorting for JobWeights

---
+++ JobWeightComparator.java
@@ -0,0 +1,15 @@
+package agentTest;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class JobWeightComparator implements Comparator<JobWeight> {
+	@Override
+	public int compare(JobWeight Ob1, JobWeight Ob2) {
+		ArrayList<Integer> A1,A2;
+		A1 = Ob1.getWeights();
+		A2 = Ob2.getWeights();
+		return (A2.get(0)*A1.get(1) - A1.get(0)*A2.get(1)); // we escape division. 
+	}
+
+}
--- WeightsList.java
+++ WeightsList.java
@@ -6,6 +6,9 @@ public class WeightsList {
 
 	ArrayList<JobWeight> jobs = new ArrayList<>();
 	
+	public void sort() {
+		jobs.sort(new JobWeightComparator());
+	}
 	public ArrayList<String> toStrings(){
 		ArrayList<String> ret = new ArrayList<>();
 		for (int i =0;i<jobs.size();i++) {
